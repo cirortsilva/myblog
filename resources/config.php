55dd2b5acc2f439926f72ee258ad1fb3d56ca987
5493D1 <arthurvasconcelos6@gmail.com>
2018-01-30 23:33:07 -0200

    Add files via upload

--- resources/config.php
+++ resources/config.php
@@ -10,17 +10,3 @@ foreach ( $config as $k => $v ){
 	define(strtoupper($k), $v);
 }
 
-
-/*
-
-<?php
-
-$config['db_host'] = 'localhost';
-$config['db_user'] = 'root';
-$config['db_pass'] = '';
-$config['db_name'] = 'travel_blog';
-
-foreach ( $config as $k => $v ) {
-	define(strtoupper($k), $v);
-}
-*/
--- resources/init.php
+++ resources/init.php
@@ -1,16 +1,16 @@
 <?php
 
-// Get the database details
+// detalhes database
 
 include_once('config.php');
 
-// Connect to Database
+// Connect Database
 
 mysql_connect('localhost', 'root', ''); 
 mysql_select_db('blog');
 
 include_once('func/blog.php');
 
-// Ref:  phpacademy - PHP Tutorials: Creating a Blog (Part 3/11) - http://www.youtube.com/watch?v=EtcPEjUIt_8
+
 
 ?>
