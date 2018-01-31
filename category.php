16cce97b853052bfc9c93e4c5e1b2f8223b977f2
5493D1 <arthurvasconcelos6@gmail.com>
2018-01-30 23:31:36 -0200

    Add files via upload

--- category.php
+++ category.php
@@ -14,14 +14,14 @@ $posts = get_posts(null, $_GET['id']);
 </head>
 
 <body>
+	<?php include "includes/_nav.php" ?>
 	<div class="container">
 		<div class="row">
 			<div class="span6 offset3">
+				
+				<br>
 				<h1>Categorias</h1>
-				<br>
-				<?php include "includes/_nav.php" ?>
 				<hr>
-				<br>
 
 				<?php
 				foreach ( $posts as $post ) {
@@ -31,15 +31,15 @@ $posts = get_posts(null, $_GET['id']);
 					?>
 					<div class="post">
 						<h2><i class="icon-quote-left">&nbsp;&nbsp;</i><a href="index.php?id=<?php echo $post['post_id']; ?>"><?php echo $post['title']; ?></a></h2>
-						<small> Postado em <?php echo date('d-m-Y h:i:s', strtotime($posts['date_posted'])); ?>
+						<small> Postado em <?php echo date('d-m-Y h:i:s', strtotime($post['date_posted'])); ?>
 							em <a href="category.php?id=<?php echo $post['category_id']; ?>"><?php echo $post['name'];?></a>
 						</small>
 						<p class="post-content"><?php echo nl2br($post['contents']);?></p>
 
 						<div class="post-functions">
 							<ul>
-								<li><a href="delete_post.php?id=<?php echo $post["post_id"]; ?>"> Apagar</a></li>
-								<li><a href="edit_post.php?id=<?php echo $post['post_id']; ?>"> Editar</a></li>
+								<li><a href="delete_post.php?id=<?php echo $post["post_id"]; ?>"> Apagar </a></li>
+								<li><a href="edit_post.php?id=<?php echo $post['post_id']; ?>"> Editar </a></li>
 							</ul>
 						</div>
 						<br />
