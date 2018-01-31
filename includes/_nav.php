09de3b78d16db3b3ea2aa1cfcd0a53e7ebb1328d
Ciro Torres <cirortsilva@gmail.com>
2018-01-30 22:31:48 -0300

    att blog includes

--- includes/_nav.php
+++ includes/_nav.php
@@ -1,10 +1,10 @@
 <?php ?>
-<nav id="nav-main" class="container">
+<nav id="nav-main" class="">
 	<ul class="container">
-		<li> <a href="index.php">Início</a></li>
+		<li> <a href="index.php">Story Board</a></li>
 		<li> <a href="category.php"></i>Categorias</a></li>
-		<li> <a href="add_post.php">Add Publicação</a></li>
-		<li> <a href="add_category.php">Add Categorias</a></li>
+		<li> <a href="add_post.php">Publicações</a></li>
+		<li> <a href="add_category.php">Adicionar Categorias</a></li>
 		<li> <a href="category_list.php">Listar Categorias</a></li>
 	</ul>
 </nav>
