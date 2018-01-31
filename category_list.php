5a0d66e5267d91b7a5f7e22aacfa79697d3f8236
5493D1 <arthurvasconcelos6@gmail.com>
2018-01-30 23:34:31 -0200

    Add files via upload

--- category_list.php
+++ category_list.php
@@ -1,6 +1,6 @@
 <?php include_once('resources/init.php') ?>
 <!DOCTYPE html>
-	<html lang="en">
+	<html lang="pt-br">
 <head>
 	<title>MyBlog</title>
 	<?php include "includes/_head.php" ?>
@@ -8,18 +8,18 @@
 
 
 <body>
+<?php include "includes/_nav.php" ?>
 <div class="container">
 	<div class="row">
 		<div class="span6 offset3">
+			<br>
 			<h1>Lista de Categorias</h1>
-			<br>
-			<?php include "includes/_nav.php" ?>
+			
 			<hr>
-			<br>
 <?php 
 foreach  ( get_categories() as $category ) {
 		?>
-		<p><a href="category.php?id=<?php echo $category['id']; ?>"><?php echo $category['name']; ?></a> - <a href="delete_category.php?id=<?php echo $category['id']; ?>">Deletar</a></p>
+		<p><a href="category.php?id=<?php echo $category['id']; ?>"><?php echo $category['name']; ?></a> - <a class="btn btn-danger" href="delete_category.php?id=<?php echo $category['id']; ?>">Deletar</a></p>
 		<?php
 		}
 ?>
