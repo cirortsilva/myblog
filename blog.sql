104e0f77aab19c22a3925fe065a0742dda65a3f5
Ciro Torres <cirortsilva@gmail.com>
2018-02-01 00:09:39 -0300

    Add files via upload

--- blog.sql
+++ blog.sql
@@ -1,49 +1,86 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.5
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jul 23, 2013 at 08:00 PM
--- Server version: 5.5.29
--- PHP Version: 5.4.10
+-- Host: 127.0.0.1
+-- Generation Time: 31-Jan-2018 às 21:22
+-- Versão do servidor: 10.1.30-MariaDB
+-- PHP Version: 5.6.33
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
--- Criação da tabela 'categories'
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categories`
 --
 
 CREATE TABLE `categories` (
-  `id` int(3) NOT NULL AUTO_INCREMENT,
-  `name` varchar(24) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  `id` int(3) NOT NULL,
+  `name` varchar(24) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Valores recebidos pela tabela 'categories'
---
-
-INSERT INTO `categories` (`id`, `name`) VALUES
-
-
---
--- Criação da tabela 'posts'
+-- Estrutura da tabela `posts`
 --
 
 CREATE TABLE `posts` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(6) NOT NULL,
   `cat_id` int(3) NOT NULL,
   `title` varchar(255) NOT NULL,
   `contents` text NOT NULL,
-  `date_posted` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
+  `date_posted` datetime NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Valores recebidos pela tabela 'posts'
+-- Indexes for dumped tables
 --
 
-INSERT INTO `posts` (`id`, `cat_id`, `title`, `contents`, `date_posted`) VALUES
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
